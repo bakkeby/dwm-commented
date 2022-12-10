8bd5f1c097a8b3e332c4e78b0872ebbb46f36761
Bakkeby <bakkeby@gmail.com>
2022-12-10 11:06:32 +0100

    Bump to 970f376.
    remove workaround for a crash with color emojis on some systems, now fixed in libXft 2.3.5
    
    https://gitlab.freedesktop.org/xorg/lib/libxft/-/blob/libXft-2.3.5/NEWS
    
    Ref.
    https://git.suckless.org/dwm/commit/970f37697358574e127019eb0ee2f5725ec05ce0.html

--- drw.c
+++ drw.c
@@ -312,22 +312,6 @@ xfont_create(Drw *drw, const char *fontname, FcPattern *fontpattern)
 		die("no font specified.");
 	}
 
-	/* Do not allow using color fonts. This is a workaround for a BadLength
-	 * error from Xft with color glyphs. Modelled on the Xterm workaround. See
-	 * https://bugzilla.redhat.com/show_bug.cgi?id=1498269
-	 * https://lists.suckless.org/dev/1701/30932.html
-	 * https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=916349
-	 * and lots more all over the internet.
-	 *
-	 * Also known as the block of code that you remove or comment out if you take a patched
-	 * libxft-bgra library to enable colour emoji.
-	 */
-	FcBool iscol;
-	if(FcPatternGetBool(xfont->pattern, FC_COLOR, 0, &iscol) == FcResultMatch && iscol) {
-		XftFontClose(drw->dpy, xfont);
-		return NULL;
-	}
-
 	/* Allocate memory for the new font. Note that Fnt is an internal structure defined in drw.h. */
 	font = ecalloc(1, sizeof(Fnt));
 	font->xfont = xfont;
@@ -866,7 +850,6 @@ drw_text(Drw *drw, int x, int y, unsigned int w, unsigned int h, unsigned int lp
 			fcpattern = FcPatternDuplicate(drw->fonts->pattern);
 			FcPatternAddCharSet(fcpattern, FC_CHARSET, fccharset);
 			FcPatternAddBool(fcpattern, FC_SCALABLE, FcTrue);
-			FcPatternAddBool(fcpattern, FC_COLOR, FcFalse);
 
 			FcConfigSubstitute(NULL, fcpattern, FcMatchPattern);
 			FcDefaultSubstitute(fcpattern);
